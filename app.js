6c5511a492b3aab45a9cb7c2ccb854d572a71621
HeroCoding <112276716+Bass-Exe@users.noreply.github.com>
2025-04-04 22:09:21 -0700

    d

---
+++ app.js
@@ -0,0 +1,16 @@
+const weatherCondition = "sunny";
+
+switch (weatherCondition) {
+    case "sunny":
+        document.getElementById("box-icon").name = "sun";
+        break;  
+    case "cloudy":
+        document.getElementById("box-icon").name = "cloudy;     
+        break;
+    case "rainy":
+        document.getElementById("box-icon").name = "rainy";
+        break;
+    case "snowy":
+        document.getElementById("box-icon").name = "snowy";
+        break;
+}

8f48dee2b9e952363bf666a34ab01b8a826df0e4
benjamin10k <bjkmanning@gmail.com>
2025-04-08 10:24:52 -0600

    DataFetch.js added

---
+++ scripts/DataFetch.js
@@ -0,0 +1,51 @@
+
+const API_KEY = 'ed1f93bc05b1f1f3bfaa19950d3ff8a6'; // Replace with API key in .env file
+
+// Get city coordinates to feed to getWeatherByCoords
+//might depricate for getForcast()
+export const getCityCoordinates = async (city) => {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`);
+        if (!response.ok) {
+            throw new Error('City not found');
+        }
+        const data = await response.json();
+        if (!data || data.length === 0) {
+            throw new Error('City not found');
+        }
+        return {
+            lat: data[0].lat,
+            lon: data[0].lon,
+        };
+    } catch (error) {
+        throw error;
+    }
+};
+
+// Get weather data by coordinates to get the weather data
+export const getWeatherByCoords = async (lat, lon) => {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`);
+        if (!response.ok) {
+            throw new Error('Weather data not found');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+//temp
+export const getForecast = async (lat, lon) => {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`);
+        if (!response.ok) {
+            throw new Error('Weather data not found');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        throw error;
+    }
+};
--- scripts/Main.js
+++ scripts/Main.js
@@ -1,7 +1,6 @@
 
 import { weatherIconsMap } from '/scripts/WeatherIcons.js';
-
-const API_KEY = 'ed1f93bc05b1f1f3bfaa19950d3ff8a6'; // Replace with API key in .env file
+import { getCityCoordinates, getForecast, getWeatherByCoords } from '/scripts/DataFetch.js';
 
 const searchBar = document.getElementById('search-bar');
 
@@ -15,53 +14,6 @@ const weatherIcon = document.getElementById('weather-icon')
 
 //4day forecast
 const forecast = document.getElementById('forecast');
-
-// Get city coordinates to feed to getWeatherByCoords
-const getCityCoordinates = async (city) => {
-    try {
-        const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`);
-        if (!response.ok) {
-            throw new Error('City not found');
-        }
-        const data = await response.json();
-        if (!data || data.length === 0) {
-            throw new Error('City not found');
-        }
-        return {
-            lat: data[0].lat,
-            lon: data[0].lon,
-        };
-    } catch (error) {
-        throw error;
-    }
-};
-
-// Get weather data by coordinates to get the weather data
-const getWeatherByCoords = async (lat, lon) => {
-    try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`);
-        if (!response.ok) {
-            throw new Error('Weather data not found');
-        }
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        throw error;
-    }
-};
-
-const getForecast = async (lat, lon) => {
-    try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`);
-        if (!response.ok) {
-            throw new Error('Weather data not found');
-        }
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        throw error;
-    }
-};
 
 // Update weather display
 //to display data:
@@ -97,7 +49,9 @@ const updateWeatherDisplay = (data) => {
 const updateForecast = () => {
     forecast.textContent = '';
 
-    const today = document.createElement('div');
+    const forecastArr = {
+        
+    };
 }
 
 const updateIcon = (data) => {
